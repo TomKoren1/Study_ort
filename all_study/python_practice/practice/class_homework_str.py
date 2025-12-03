f29b66387184c4e3a5f136378359aa58946ed7c7
tom_laptop <dingdongtaken@gmail.com>
2025-12-03 14:44:53 +0200

    python home works 03/12/25 done

---
+++ all_study/python_practice/practice/class_homework_str.py
@@ -0,0 +1,96 @@
+def ex1a(s):
+    count=0
+    for i,c in enumerate(s):
+        if c=='a':
+            for j in range(i,len(s)):
+                if s[j]=='b':
+                    count+=1
+    print(f'number of sequences whre it start with a and end with b: {count}')
+def ex1b(s):
+    count=0
+    for i,c in enumerate(s):
+        if c=='a':
+            for j in range(i,len(s)):
+                if s[j]=='b':
+                    count+=1
+                if s[j]=='c':
+                    break
+    print(f'number of sequences whre it start with a and end with b and dosent contain c between: {count}')  
+def ex1c(s):
+    count=0
+    for i,c in enumerate(s):
+        if c=='a':
+            c_count=0
+            for j in range(i,len(s)):
+                if s[j]=='b' and c_count==1:
+                    count+=1
+                if s[j]=='c':
+                    c_count+=1
+                if c_count>1:
+                    break
+    print(f'number of sequences whre it start with a and end with b with one c between: {count}')     
+def ex1():
+    inp=''
+    while(inp!='.'):
+        s+=inp
+        inp=input('enter a character from abc: ')
+    ex1a(s)
+    ex1b(s)
+    ex1c(s)
+
+def ex2():
+    all_str='all the strings that start and end in the same character:'
+    for i in range(15):
+        s=input(f'enter string number {i}: ')
+        if s[0]==s[len(s)-1]:
+            all_str+=f' {s},'
+    print(all_str[:len(all_str)-1])
+
+def ex3(s1,s2):
+    min_str=s1
+    max_str=s2
+    if len(s1)>len(s2):
+        max_str=s1
+        min_str=s2
+    if len(max_str)>len(min_str)*2:
+        print(max_str)
+    else:
+        print(min_str)
+
+def ex4(s1,s2):
+    count=0
+    for i,c in enumerate(s1):
+        if c== s2[0]:
+            for j in range(len(s2)):
+                if s1[i+j]!=s2[j]:
+                    break
+                if j==len(s2)-1:
+                    count+=1
+    print(count)
+
+def ex5(s):
+    sum=0
+    num=0
+    for c in s:
+        if c>='0' and c<='9':
+            num=num*10+int(c)
+        else:
+            sum+=num
+            num=0
+    print(sum)
+def ex6(s,seperator):
+    new_s=''
+    for c in s:
+        if c != seperator:
+            new_s+=c
+    print(new_s)
+
+def ex7(s,key):
+    new_s=''
+    for i,c in enumerate(s):
+        if(ord(c)+key<=ord('Z')):
+            new_s+=chr(ord(c)+key)
+        else:
+            new_s+=chr(ord('A')+key-ord('Z')+ord(c)-1)
+    print(new_s)
+ex7('BABY',3)
---
+++ all_study/python_practice/practice/practice_in_class03-12-25.py
@@ -0,0 +1,36 @@
+import random
+
+def guessing_game():
+    d1=str(random.randint(0,9))
+    d2=str(random.randint(0,9))
+    d3=str(random.randint(0,9))
+    d4=str(random.randint(0,9))
+    full_random=d1+d2+d3+d4
+    guessed=False
+    while not guessed:
+        guess=input('enter your guess: ')
+        valid_guess=True
+        if len(guess)!=4:
+            valid_guess=False
+            print('not the right lenght of guess (you need 4)')
+        for c in guess:
+            if c>'9' or c<'0':
+                valid_guess=False
+                print('not correct characters')
+        if valid_guess:
+            answer=''
+            if guess == full_random:
+                answer=('you guessed right!!!')
+                guessed=True
+            else:
+                for i in range(4):
+                    if(full_random[i]==guess[i]):
+                        answer+='+'
+                    if guess[i] in full_random and full_random[i]!=guess[i]:
+                        answer+='*'
+                    if guess[i] not in full_random:
+                        answer+='-'
+            print(answer)
+
+
+guessing_game()
