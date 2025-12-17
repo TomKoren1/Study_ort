bb5b88089959d85e51e832ebd757efd6cf1787b6
tom_laptop <dingdongtaken@gmail.com>
2025-12-17 12:25:57 +0200

    added some python practices

--- all_study/python_practice/practice/practice_in_class15-12-25_rekursia.py
+++ all_study/python_practice/practice/practice_in_class15-12-25_rekursia.py
@@ -27,4 +27,35 @@ def fibo(end):
     print(f"{n1}, {n2}")
     return n2
 
-fibo(300)
+def sum_of_array(arr):
+    if len(arr)>0:
+        new_list=arr
+        return new_list.pop() + sum_of_array(new_list)
+    return 0
+def sum_of_array_2(arr):
+    if len(arr)>0:
+        return arr[0]+sum_of_array_2(arr[1:])
+    else:
+        return 0
+
+def merge_3_array(a,b,c):
+    i1=0
+    i2=0
+    while i1<len(a) and i2<len(b):
+        if a[i1]<=b[i2]:
+            c.append(a[i1])
+            i1+=1
+        else:
+            c.append(b[i2])
+            i2+=1
+    while i1<len(a):
+        c.append(a[i1])
+        i1+=1
+    while i2<len(b):
+        c.append(b[i2])
+        i2+=1
+    
+    print(c)
+
+merge_3_array([1,2,4,10],[1,8,9],[])
+
